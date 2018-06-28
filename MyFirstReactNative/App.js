e71c36eeb04ae3b914e3b0949316d9139f2e1a3e
Kevin Ray Ermac <ermac.kevin@gmail.com>
2018-06-28 18:10:33 +0800

    import React, { Component } from 'react'; import PropTypes from 'prop-types'; import { TouchableOpacity, Text, StyleSheet } from 'react-native';
    class customButton extends Component {
    	render() {
    		const { text, onPress} = this.props;
    		return (
    		  <TouchableOpacity style={styles.buttonStyle}
    			onPress={() => onPress()}
    		  >
    			 <Text style={styles.textStyle}>{text}</Text>
    		  </TouchableOpacity>
    		);
    	}
    }

--- MyFirstReactNative/App.js
+++ MyFirstReactNative/App.js
@@ -10,7 +10,8 @@ import {
   ScrollView, 
   KeyboardAvoidingView,
   TouchableOpacity,
-  Alert  } from 'react-native';
+  Alert,
+  Platfrom} from 'react-native';
   import CustomButton from './Components/button'
   import HideMe from './Components/hideMe';
 
@@ -18,21 +19,41 @@ export default class Vananaz extends Component {
 
   constructor(props) {
     super(props);
+
     this.state={
       username:"",
       password: "",
       passwordLength: 0,
+      passwordError: 'valid',
       isHidden: false,
       isError: false,
     }
   }
 
+/*Gets the length of the password*/
+  _getPasswordLength = (ValueHolder) => {
+    var Value=ValueHolder.length.toString();
+    this.setState({passwordLength: Value})
+  }
 
+/*Validates the password entered. 6-12 characters only.*/
+  _validatePassword = () => {
+    var Value = this.state.passwordLength;
+    if (Value>=6 && Value<=12)
+    {
+      this.setState({passwordError: 'Valid'})
+      console.log('Password is valid')
+    }
+    else
+    { 
+      this.setState({passwordError: 'Invalid'})
+      console.log('Password is invalid')
+    }
+  }
 
-  _onPressButton = () => {
+/*Validates the email address entered.*/
+  _validateEmail = () => {
     let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ ;
-    console.log(this.state.isError)
-    console.log(this.state.username);
       if(reg.test(this.state.username) === false)
       {
         console.log("Email is Not Correct");
@@ -42,13 +63,16 @@ export default class Vananaz extends Component {
       {
         console.log("Email is Correct");
         this.setState({isError: false})
-      }
+      }  
+  }
 
-    
-    
+  _onPressButton = () => {
+    this._validateEmail();
+    this._validatePassword();
+      console.log('Email: ' + this.state.username);
+      console.log('Password Lenght: ' + this.state.passwordLength);
   }
   
-
   render() {
     return (
         
@@ -68,37 +92,39 @@ export default class Vananaz extends Component {
             underlineColorAndroid='transparent'
             style={styles.input}
             placeholder ='Input email address'
-
             onChangeText={
-              username => this.setState({username})
+              (text) => {
+                this.setState({username: text})
+              }
             }
-          
-            
-          
           />
+
           <Text style={styles.error}>not correct format for email address</Text>
 
-          <Text style={styles.label}>You type: {this.state.username} </Text>
+          <Text style={styles.label}>Password</Text>
           <TextInput
             underlineColorAndroid='transparent'
             secureTextEntry={true} 
             style={styles.input}
             placeholder ='Input password'
-
             onChangeText={
-              (text) => {
-                this.setState({password: text});
+                (text) => {
+                  this.setState({password: text}),
+                  this._getPasswordLength(text)
               }
             }
-          
           />
+
           <Text style={styles.error}>please use at least 6-12 characters</Text>
+
         </View>
 
         <View style={styles.buttonContainer}>
           <CustomButton 
                   text="Sign In"
-                  onPress={this._onPressButton}
+                  onPress={
+                    this._onPressButton}
+
                   />
         </View>
         </KeyboardAvoidingView>
@@ -136,7 +162,7 @@ const styles = StyleSheet.create({
   },
 
   error: {
-    color: 'red',
+    color: 'white',
     fontSize: 10,
     fontStyle: 'italic',
   },
